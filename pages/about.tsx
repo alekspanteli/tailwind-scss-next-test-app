06d760db796ebf83d8ed04d7816e7a89d62f655c
alekspanteli <alekspanteli@rambler.ru>
2022-12-12 10:50:02 +0000

    create starter template

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,16 +1,16 @@
 import Head from "next/head";
 import Link from 'next/link'
 
-export default function Home() {
+export default function About() {
   return (
     <>
       <Head>
-        <title>Home</title>
+        <title>About</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <div className="wrapper">
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        <Link href="/about">About</Link>
+        <Link href="/">Home</Link>
       </div>
     </>
   );
