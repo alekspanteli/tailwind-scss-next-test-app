a6acf4cac797e9cf070fafacc33ba1cc7ecb19c7
alekspanteli <alekspanteli@rambler.ru>
2022-12-12 10:35:37 +0000

    add head

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,16 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
-    <div className="wrapper">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <p className="test">test</p>
-    </div>
-  )
+    <>
+      <Head>
+        <title>Home</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div className="wrapper">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <p className="test">test</p>
+      </div>
+    </>
+  );
 }
