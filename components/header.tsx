3fdc49b154745ee5cab7a950ea6ddc5713df00aa
alekspanteli <alekspanteli@rambler.ru>
2022-12-13 12:10:23 +0000

    add where selector and active link

--- components/header.tsx
+++ components/header.tsx
@@ -1,28 +1,55 @@
-import Link from 'next/link'
+import Link from "next/link";
+import { clsx } from "clsx";
+import { useRouter } from "next/router";
 
 export default function Header() {
+  const router = useRouter();
   return (
     <header>
       <h1>Header goes here</h1>
       <nav className="primary-navigation" aria-label="Primary">
         <ul className="primary-navigation__list">
           <li className="primary-navigation__item">
-            <Link href="/" className="primary-navigation__link">
+            <Link
+              href="/"
+              className={clsx(
+                "primary-navigation__link",
+                router.pathname == "/" ? "underline" : ""
+              )}
+            >
               Home
             </Link>
           </li>
           <li className="primary-navigation__item">
-            <Link href="/about" className="primary-navigation__link">
+            <Link
+              href="/about"
+              className={clsx(
+                "primary-navigation__link",
+                router.pathname == "/about" ? "underline" : ""
+              )}
+            >
               About
             </Link>
           </li>
           <li className="primary-navigation__item">
-            <Link href="/nesting" className="primary-navigation__link">
+            <Link
+              href="/nesting"
+              className={clsx(
+                "primary-navigation__link",
+                router.pathname == "/nesting" ? "underline" : ""
+              )}
+            >
               Nesting
             </Link>
           </li>
           <li className="primary-navigation__item">
-            <Link href="/nesting-challenge-3" className="primary-navigation__link">
+            <Link
+              href="/nesting-challenge-3"
+              className={clsx(
+                "primary-navigation__link",
+                router.pathname == "/nesting-challenge-3" ? "underline" : ""
+              )}
+            >
               Nesting 3 Challange
             </Link>
           </li>
