56d079e702b3cb6843063b6d03f6fe6986c88dee
alekspanteli <alekspanteli@rambler.ru>
2022-12-14 10:36:14 +0000

    wip image

--- pages/image.tsx
+++ pages/image.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import styles from "@/pages/image.module.scss";
 
 export default function About() {
   return (
@@ -17,7 +18,7 @@ export default function About() {
         src="/images/elva-fairy-800w.jpg"
         alt="Elva dressed as a fairy"
       /> */}
-      <Image
+      {/* <Image
         src="/images/elva-fairy-800w.jpg"
         alt="test"
         width={200}
@@ -25,7 +26,18 @@ export default function About() {
         sizes="(max-width: 768px) 100vw,
               (max-width: 1200px) 50vw,
               33vw"
-      />
+      /> */}
+      <div className={styles.wrapper}>
+        <img
+          src="/images/cat-500.jpg"
+          className={styles.myimage}
+          srcSet="/images/cat-500.jpg 500w,
+                /images/cat-1000.jpg 1000w,
+                /images/cat-1500.jpg 1500w"
+          alt="Test image"
+        />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem ex quaerat obcaecati tempora! Atque, temporibus ullam voluptate neque ipsa minus accusantium quae laudantium voluptatibus labore, adipisci praesentium, minima odit harum!</p>
+      </div>
     </>
   );
 }
