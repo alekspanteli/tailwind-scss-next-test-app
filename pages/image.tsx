e2a22f117a24ffae9fad9003091f7279632518a2
alekspanteli <alekspanteli@rambler.ru>
2022-12-14 14:56:30 +0000

    finish working

--- pages/image.tsx
+++ pages/image.tsx
@@ -11,13 +11,13 @@ export default function Images() {
       </Head>
       <h1 className="text-3xl font-bold underline">Images page</h1>
       {/* <img src="/images/elva-fairy-320w.jpg" /> */}
-      {/* <img
-        srcSet="/images/elva-fairy-800w.jpg 800w, /images/elva-fairy-480w.jpg 480w"
-        sizes="(min-width: 600px) 800px, 480px
+      <img
+        srcSet="/images/elva-fairy-480w.jpg 480w, /images/elva-fairy-800w.jpg 800w"
+        sizes="(min-width: 1000px) 800px, 480px
          "
         src="/images/elva-fairy-800w.jpg"
         alt="Elva dressed as a fairy"
-      /> */}
+      />
       {/* <Image
         src="/images/elva-fairy-800w.jpg"
         alt="test"
@@ -27,25 +27,15 @@ export default function Images() {
               (max-width: 1200px) 50vw,
               33vw"
       /> */}
-      {/* <div className={styles.wrapper}>
-        <img
+      {/* <img
           src="/images/cat-500.jpg"
           className={styles.myimage}
           srcSet="/images/cat-500.jpg 500w,
                 /images/cat-1000.jpg 1000w,
                 /images/cat-1500.jpg 1500w"
-          sizes="(min-width: 760px) calc(50vw - 2em), 100vw"
+          sizes="(min-width: 760px) 1000px, 100vw"
           alt="Test image"
-        />
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem ex quaerat obcaecati tempora! Atque, temporibus ullam voluptate neque ipsa minus accusantium quae laudantium voluptatibus labore, adipisci praesentium, minima odit harum!</p>
-      </div> */}
-      <img
-        srcSet="/images/elva-fairy-480w.jpg 480w, /images/elva-fairy-800w.jpg 800w"
-        sizes="(min-width: 600px) 480px
-         "
-        src="/images/elva-fairy-800w.jpg"
-        alt="Elva dressed as a fairy"
-      /> 
+        /> */}
     </>
   );
 }
